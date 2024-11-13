abb1e483cb0eda3967ada1072a7f7419eee8b22c
Modasser Hasan <manvstech2.0@gmail.com>
2024-11-13 23:46:02 +0600

    Footer design updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,61 +2,95 @@ import { Link } from "react-router-dom";
 import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
-    const ti = (
-        <div>
-            <h5 className="font-semibold mb-2 sm:mb-5">About Us</h5>
-            <ul className="space-y-0.5 sm:space-y-2 text-gray-600">
-                <li>
-                    <Link to={"#"}></Link>
-                </li>
-            </ul>
-        </div>
-    );
-
     return (
         <footer className="max-width !px-5 py-5">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 pb-5 mb-5 border-b border-gray-300 text-center gap-7 sm:text-start md:gap-0">
                 <div>
                     <h5 className="font-semibold mb-2 sm:mb-5">About Us</h5>
-                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600">
+                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600 text-sm">
                         <li>
-                            <Link to={"#"}>Administration contacts</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Administration contacts
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>Career</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Career
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>Quality assurance</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Quality assurance
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>Privacy Policy</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Privacy Policy
+                            </Link>
                         </li>
                     </ul>
                 </div>
 
                 <div>
                     <h5 className="font-semibold mb-2 sm:mb-5">Current</h5>
-                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600">
+                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600 text-sm">
                         <li>
-                            <Link to={"#"}>Preparation for research</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Preparation for research
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>Buying and Selling rules</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Buying and Selling rules
+                            </Link>
                         </li>
                     </ul>
                 </div>
 
                 <div>
                     <h5 className="font-semibold mb-2 sm:mb-5">Related</h5>
-                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600">
+                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600 text-sm">
                         <li>
-                            <Link to={"#"}>How to Buy?</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                How to Buy?
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>FAQ</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                FAQ
+                            </Link>
                         </li>
                         <li>
-                            <Link to={"#"}>Cookie Policy</Link>
+                            <Link
+                                to={"#"}
+                                className="hover:underline underline-offset-2"
+                            >
+                                Cookie Policy
+                            </Link>
                         </li>
                     </ul>
                 </div>
@@ -65,7 +99,7 @@ const Footer = () => {
                     <h5 className="font-semibold mb-2 sm:mb-5">
                         Company Details
                     </h5>
-                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600">
+                    <ul className="space-y-0.5 sm:space-y-2 text-gray-600 text-sm">
                         <li>UAB Diagnostic Laboratory</li>
                         <li>37 Main Road, Dhaka</li>
                         <li>Company Code: 30000033</li>
@@ -73,13 +107,13 @@ const Footer = () => {
                         <li className="flex justify-center pt-2 sm:p-0 sm:justify-start gap-2">
                             <a
                                 href="#"
-                                className="text-4xl text-gray-500 hover:text-primary"
+                                className="text-4xl text-gray-400 hover:text-primary"
                             >
                                 <FaFacebookSquare />
                             </a>
                             <a
                                 href="#"
-                                className="text-4xl text-gray-500 hover:text-primary"
+                                className="text-4xl text-gray-400 hover:text-primary"
                             >
                                 <FaLinkedin />
                             </a>
@@ -89,7 +123,7 @@ const Footer = () => {
             </div>
 
             <p className="flex flex-col sm:flex-row gap-1 justify-between items-center text-gray-600 text-sm">
-                <span>&copy;2024 All rights reserved. Anteja</span>
+                <span>&copy; 2024 All rights reserved. Anteja</span>
                 <span>
                     Design and Programming:{" "}
                     <a
